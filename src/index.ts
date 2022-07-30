a5ba308f09c34e8e15c9016a8acb880ff0724681
XLor <yjl9903@vip.qq.com>
2022-07-30 16:49:15 +0000

    fix: export types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import type { AppOption } from './types';
 
 import { Breadc } from './breadc';
 
+export type { Breadc };
+
+export type { Command, CommandConfig } from './command';
+
+export type { Option, OptionConfig } from './option';
+
 export default function breadc<T extends object = {}>(
   name: string,
   option: AppOption = {}
