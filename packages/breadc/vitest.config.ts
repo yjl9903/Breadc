dc85e3e7c22636e5fc26727b2a4e84797e03d1c0
XLor <yjl9903@vip.qq.com>
2023-02-04 21:44:06 +0800

    chore: enable codecov

--- packages/breadc/vitest.config.ts
+++ packages/breadc/vitest.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
-      provider: 'c8'
+      provider: 'c8',
+      reportsDirectory: 'coverage',
+      reporter: ['text', 'html', 'json', 'lcov']
     }
   }
 });
