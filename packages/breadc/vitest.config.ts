409713c63c91a9c42fb61cb35b5cb2f108fc283e
XLor <yjl9903@vip.qq.com>
2023-12-06 17:52:16 +0800

    fix(breadc): exclude mod.ts from coverage

--- packages/breadc/vitest.config.ts
+++ packages/breadc/vitest.config.ts
@@ -5,7 +5,26 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reportsDirectory: 'coverage',
-      reporter: ['text', 'html', 'json', 'lcov']
+      reporter: ['text', 'html', 'json', 'lcov'],
+      exclude: [
+        '**/mod.ts',
+        'coverage/**',
+        'dist/**',
+        '**/[.]**',
+        'packages/*/test?(s)/**',
+        '**/*.d.ts',
+        '**/virtual:*',
+        '**/__x00__*',
+        '**/\x00*',
+        'cypress/**',
+        'test?(s)/**',
+        'test?(-*).?(c|m)[jt]s?(x)',
+        '**/*{.,-}{test,spec}.?(c|m)[jt]s?(x)',
+        '**/__tests__/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+        '**/vitest.{workspace,projects}.[jt]s?(on)',
+        '**/.{eslint,mocha,prettier}rc.{?(c|m)js,yml}'
+      ]
     }
   }
 });
