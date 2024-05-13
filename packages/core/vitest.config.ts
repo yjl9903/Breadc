61077055cdf5c9e43420e7c6a1f3313157da1249
XLor <yjl9903@vip.qq.com>
2024-05-13 23:05:53 +0800

    test: add core coverage

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -4,6 +4,30 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   test: {
-    includeSource: ['src/**/*.{js,ts}']
+    includeSource: ['src/**/*.{js,ts}'],
+    coverage: {
+      provider: 'v8',
+      reportsDirectory: 'coverage',
+      reporter: ['text', 'html', 'json', 'lcov'],
+      exclude: [
+        '**/mod.ts',
+        'coverage/**',
+        'dist/**',
+        '**/[.]**',
+        'packages/*/test?(s)/**',
+        '**/*.d.ts',
+        '**/virtual:*',
+        '**/__x00__*',
+        '**/\x00*',
+        'cypress/**',
+        'test?(s)/**',
+        'test?(-*).?(c|m)[jt]s?(x)',
+        '**/*{.,-}{test,spec}.?(c|m)[jt]s?(x)',
+        '**/__tests__/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+        '**/vitest.{workspace,projects}.[jt]s?(on)',
+        '**/.{eslint,mocha,prettier}rc.{?(c|m)js,yml}'
+      ]
+    }
   }
 });
