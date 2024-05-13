347ebf0bcce6aaf663f1b3ee73a057e23cac61bb
XLor <yjl9903@vip.qq.com>
2024-05-14 00:20:08 +0800

    feat(core): add command and option def

--- packages/core/src/breadc/app.ts
+++ packages/core/src/breadc/app.ts
@@ -1,6 +1,9 @@
 import type { BreadcOptions } from './types.ts';
 
-export class Breadc {
+import { Option } from './option.ts';
+import { Command } from './command.ts';
+
+export class Breadc<GO extends object = {}> {
   public name: string;
 
   public version: string | undefined = undefined;
@@ -13,14 +16,28 @@ export class Breadc {
 
   // --- Builder ---
 
-  // TODO
-  public option() {}
+  public option<F extends string>(format: F): Breadc<GO> {
+    const option = new Option<F>(format);
+    // TODO
+    return this;
+  }
+
+  public command<F extends string>(format: F): Command<F> {
+    // '' / '[...]' / '<...>' should be treated as the default command
+    if (format.length === 0 || format[0] === '[' || format[0] === '<') {
+      return this.default(format);
+    }
+    const command = new Command<F>(format);
+    // TODO
+    return command;
+  }
 
   // TODO
-  public command(command: string) {}
-
-  // TODO
-  public default() {}
+  private default<F extends string>(format: F): Command<F> {
+    const command = new Command<F>(format);
+    // TODO
+    return command;
+  }
 
   // --- Parse ---
 
