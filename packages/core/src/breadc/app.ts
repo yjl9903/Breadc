d3b5164181b66d5f654e7449e0bef19a01d896d0
XLor <yjl9903@vip.qq.com>
2024-07-02 23:45:20 +0800

    feat(core): more context

--- packages/core/src/breadc/app.ts
+++ packages/core/src/breadc/app.ts
@@ -1,6 +1,7 @@
-import { Context } from '../parser/context.ts';
-
 import type { BreadcOptions } from './types.ts';
+
+import { parse, run } from '../parser/index.ts';
+import { type Container, Context } from '../parser/context.ts';
 
 import { Option } from './option.ts';
 import { Command } from './command.ts';
@@ -12,6 +13,11 @@ export class Breadc<GO extends object = {}> {
 
   public description: string | undefined = undefined;
 
+  private container: Container = {
+    options: [],
+    commands: []
+  };
+
   public constructor(name: string, options: BreadcOptions = {}) {
     this.name = name;
   }
@@ -19,47 +25,52 @@ export class Breadc<GO extends object = {}> {
   // --- Builder ---
 
   public addOption<F extends string>(option: Option<F>): Breadc<GO> {
-    // TODO
+    this.container.options.push(option);
     return this;
   }
 
   public option<F extends string>(format: F): Breadc<GO> {
     const option = new Option<F>(format);
-    // TODO
+    this.container.options.push(option);
     return this;
   }
 
   public addCommand<F extends string>(command: Command<F>): Breadc<GO> {
-    // TODO
+    this.container.commands.push(command);
     return this;
   }
 
   public command<F extends string>(format: F): Command<F> {
     // '' / '[...]' / '<...>' should be treated as the default command
-    if (format.length === 0 || format[0] === '[' || format[0] === '<') {
-      return this.default(format);
-    }
+    // if (format.length === 0 || format[0] === '[' || format[0] === '<') {
+    //   return this.default(format);
+    // }
     const command = new Command<F>(format);
-    // TODO
+    this.container.commands.push(command);
     return command;
   }
 
-  // TODO
-  private default<F extends string>(format: F): Command<F> {
-    const command = new Command<F>(format);
-    // TODO
-    return command;
-  }
+  // --- Parse / Run ---
 
-  // --- Parse ---
-
-  // TODO
+  /**
+   * Parse the arguments only
+   *
+   * @param args input arguments
+   * @returns the parsed context
+   */
   public parse(args: string[]) {
-    const context = new Context(args);
+    const context = new Context(this.container, args);
+    return parse(context);
   }
 
-  // TODO
-  public async run(args: string[]) {
-    const context = new Context(args);
+  /**
+   * Parse the arguments and run the the corresponding action function
+   *
+   * @param args input arguments
+   * @returns the returned value from the corresponding action function
+   */
+  public async run<T = unknown>(args: string[]): Promise<T> {
+    const context = this.parse(args);
+    return run(context);
   }
 }
