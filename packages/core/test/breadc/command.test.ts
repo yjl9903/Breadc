8fa108f567625237bbb9d206ed900a9f912f2933
yjl9903 <yjl9903@onekuma.cn>
2024-07-12 18:28:54 +0800

    feat(core): more command resolve testcases

--- packages/core/test/breadc/command.test.ts
+++ packages/core/test/breadc/command.test.ts
@@ -40,5 +40,107 @@ describe('command', () => {
     expect(cmd.spread).toMatchInlineSnapshot(`"rest"`);
   });
 
-  // TODO: test errors
+  it('should resolve duplicated spaces', () => {
+    const cmd = new Command('submodule   add   <abc>   [def]   [...rest]');
+    cmd.resolve();
+    cmd.resolve();
+    expect(cmd.pieces).toMatchInlineSnapshot(`
+      [
+        "submodule",
+        "dd",
+        "abc>",
+        "def]",
+        "...rest]",
+      ]
+    `);
+    expect(cmd.required).toMatchInlineSnapshot(`[]`);
+    expect(cmd.optionals).toMatchInlineSnapshot(`[]`);
+    expect(cmd.spread).toMatchInlineSnapshot(`undefined`);
+  });
+
+  it('should find invalid required arguments', () => {
+    expect(async () => {
+      const cmd = new Command('submodule add <');
+      cmd.resolve().resolve();
+    }).rejects.toThrowErrorMatchingInlineSnapshot(
+      `[Error: Resolving invalid required argument at the command "submodule add <", position 14]`
+    );
+
+    expect(async () => {
+      const cmd = new Command('submodule add < [def]');
+      cmd.resolve().resolve();
+    }).rejects.toThrowErrorMatchingInlineSnapshot(
+      `[Error: Resolving invalid required argument at the command "submodule add < [def]", position 14]`
+    );
+
+    expect(async () => {
+      const cmd = new Command('submodule add <abc [def]');
+      cmd.resolve().resolve();
+    }).rejects.toThrowErrorMatchingInlineSnapshot(
+      `[Error: Resolving invalid required argument at the command "submodule add <abc [def]", position 24]`
+    );
+
+    expect(async () => {
+      const cmd = new Command('submodule add [abc] <def>');
+      cmd.resolve().resolve();
+    }).rejects.toThrowErrorMatchingInlineSnapshot(
+      `[Error: Required argument should be placed before optional arguments at the command "submodule add [abc] <def>", position 21]`
+    );
+  });
+
+  it('should find invalid optional arguments', () => {
+    expect(async () => {
+      const cmd = new Command('submodule add [');
+      cmd.resolve().resolve();
+    }).rejects.toThrowErrorMatchingInlineSnapshot(
+      `[Error: Resolving invalid optional argument at the command "submodule add [", position 14]`
+    );
+
+    expect(async () => {
+      const cmd = new Command('submodule add [ [def]');
+      cmd.resolve().resolve();
+    }).rejects.toThrowErrorMatchingInlineSnapshot(
+      `[Error: Resolving invalid optional argument at the command "submodule add [ [def]", position 14]`
+    );
+
+    expect(async () => {
+      const cmd = new Command('submodule add [abc');
+      cmd.resolve().resolve();
+    }).rejects.toThrowErrorMatchingInlineSnapshot(
+      `[Error: Resolving invalid optional argument at the command "submodule add [abc", position 18]`
+    );
+
+    {
+      const cmd = new Command('submodule add [abc [def]');
+      cmd.resolve().resolve();
+      expect(cmd.optionals).toMatchInlineSnapshot(`
+        [
+          "abc [def",
+        ]
+      `);
+    }
+  });
+
+  it('should find invalid spread arguments', () => {
+    expect(async () => {
+      const cmd = new Command('submodule add [...');
+      cmd.resolve().resolve();
+    }).rejects.toThrowErrorMatchingInlineSnapshot(
+      `[Error: Resolving invalid spread argument at the command "submodule add [...", position 18]`
+    );
+
+    expect(async () => {
+      const cmd = new Command('submodule add [... <abc>');
+      cmd.resolve().resolve();
+    }).rejects.toThrowErrorMatchingInlineSnapshot(
+      `[Error: Resolving invalid spread argument at the command "submodule add [... <abc>", position 24]`
+    );
+
+    expect(async () => {
+      const cmd = new Command('submodule add [...rest] [abc]');
+      cmd.resolve().resolve();
+    }).rejects.toThrowErrorMatchingInlineSnapshot(
+      `[Error: Optional argument should be placed before spread arguments at the command "submodule add [...rest] [abc]", position 25]`
+    );
+  });
 });
