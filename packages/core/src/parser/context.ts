f9cd85634f92cb405b6bcfea7041b60e14a47fa1
XLor <yjl9903@vip.qq.com>
2024-07-15 23:09:48 +0800

    refactor(core): some parse logic

--- packages/core/src/parser/context.ts
+++ packages/core/src/parser/context.ts
@@ -1,7 +1,6 @@
 import type { Option } from '../breadc/option.ts';
 import type { Command } from '../breadc/command.ts';
 
-import { Parser } from './parser.ts';
 import { Lexer, Token } from './lexer.ts';
 
 export interface Container {
@@ -11,24 +10,47 @@ export interface Container {
 }
 
 export class Context {
+  /**
+   * The metadata, options, commands registed from the Breadc app instance
+   */
   public readonly container: Container;
 
   public readonly lexer: Lexer;
 
-  public readonly parser: Parser;
-
+  /**
+   * Matched command
+   */
   public command: Command | undefined = undefined;
 
+  /**
+   * Matched arguments
+   */
   public readonly arguments: MatchedArgument[] = [];
 
+  /**
+   * Matched options
+   */
   public readonly options: Map<Option, MatchedOption> = new Map();
 
+  /**
+   * Remaining arguments
+   */
   public readonly remaining: Token[] = [];
+
+  /**
+   * Pending commands and options which are used internal
+   */
+  public readonly matching: {
+    commands: Map<string, Command[]>;
+    options: Map<string, Option>;
+  } = {
+    commands: new Map(),
+    options: new Map()
+  };
 
   public constructor(container: Container, args: string[]) {
     this.container = container;
     this.lexer = new Lexer(args);
-    this.parser = new Parser(this.lexer);
   }
 }
 
