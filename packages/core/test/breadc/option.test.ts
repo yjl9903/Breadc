f24f3cc86e51db82deb7f8c63c8ff3ff3a37b072
yjl9903 <yjl9903@onekuma.cn>
2024-07-15 19:18:16 +0800

    feat(core): resolve option

--- packages/core/test/breadc/option.test.ts
+++ packages/core/test/breadc/option.test.ts
@@ -3,18 +3,87 @@ import { describe, it, expect } from 'vitest';
 import { Option } from '../../src/breadc/option.ts';
 
 describe('option', () => {
-  it('should resolve short option', () => {
-    const opt = new Option('-s');
+  it('should resolve short and long option', () => {
+    const opt = new Option('-v, --version');
     opt.resolve();
+    expect(opt).toMatchInlineSnapshot(`
+      Option {
+        "format": "-v, --version",
+        "long": "version",
+        "name": undefined,
+        "resolved": true,
+        "short": "v",
+        "type": "boolean",
+      }
+    `);
   });
 
   it('should resolve long option', () => {
-    const opt = new Option('-s');
+    const opt = new Option('--version');
     opt.resolve();
+    expect(opt).toMatchInlineSnapshot(`
+      Option {
+        "format": "--version",
+        "long": "version",
+        "name": undefined,
+        "resolved": true,
+        "short": undefined,
+        "type": "boolean",
+      }
+    `);
   });
 
-  it('should resolve short and long option', () => {
-    const opt = new Option('-s');
+  it('should not resolve short option', () => {
+    const opt = new Option('-v');
+    expect(async () =>
+      opt.resolve()
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `[Error: Resolving invalid option at the option "-v"]`
+    );
+  });
+
+  it('should resolve required option', () => {
+    const opt = new Option('--root <root>');
     opt.resolve();
+    expect(opt).toMatchInlineSnapshot(`
+      Option {
+        "format": "--root <root>",
+        "long": "root",
+        "name": "<root>",
+        "resolved": true,
+        "short": undefined,
+        "type": "required",
+      }
+    `);
+  });
+
+  it('should resolve optional option', () => {
+    const opt = new Option('--root [root]');
+    opt.resolve();
+    expect(opt).toMatchInlineSnapshot(`
+      Option {
+        "format": "--root [root]",
+        "long": "root",
+        "name": "[root]",
+        "resolved": true,
+        "short": undefined,
+        "type": "optional",
+      }
+    `);
+  });
+
+  it('should resolve array option', () => {
+    const opt = new Option('--plugin [...plugins]');
+    opt.resolve();
+    expect(opt).toMatchInlineSnapshot(`
+      Option {
+        "format": "--plugin [...plugins]",
+        "long": "plugin",
+        "name": "[...plugins]",
+        "resolved": true,
+        "short": undefined,
+        "type": "array",
+      }
+    `);
   });
 });
