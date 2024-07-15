1b371511b593a885580ad9c607e14c1c3172dfb6
yjl9903 <yjl9903@onekuma.cn>
2024-07-15 12:05:02 +0800

    feat(core): init options

--- packages/core/src/breadc/option.ts
+++ packages/core/src/breadc/option.ts
@@ -1,5 +1,16 @@
+import { BreadcError } from '../error.ts';
+
+/**
+ * Option abstraction
+ *
+ * - -s <name>
+ * - --long <name>
+ * - -s, --long <name>
+ */
 export class Option<F extends string = string> {
   public readonly format: F;
+
+  private resolved = false;
 
   public constructor(format: F) {
     this.format = format;
@@ -11,6 +22,21 @@ export class Option<F extends string = string> {
    * @returns this
    */
   public resolve(): this {
+    if (this.resolved) return this;
+
+    this.resolved = true;
+
     return this;
+  }
+}
+
+export class ResolveOptionError extends BreadcError {
+  public constructor(
+    message: string,
+    cause: { format: string; position: number }
+  ) {
+    super(
+      `${message} at the option "${cause.format}", position ${cause.position}`
+    );
   }
 }
