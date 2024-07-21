8639251906a6e24fd5951b8ba7f58f300ff51874
XLor <yjl9903@vip.qq.com>
2024-07-22 01:29:16 +0800

    chore: fix command action

--- packages/core/src/parser/index.ts
+++ packages/core/src/parser/index.ts
@@ -5,14 +5,14 @@ import { Context } from './context.ts';
 export { parse } from './parser.ts';
 
 export function run(context: Context): Promise<any> {
-  if (context.command && context.command.actionFn) {
+  if (context.command && context.command.command.actionFn) {
     const args = context.arguments.map((ma) => ma.value);
     const options = Object.fromEntries(
       [...context.options.entries()].map(
         ([key, mo]) => [key, mo.value] as const
       )
     );
-    return context.command.actionFn(...args, options);
+    return context.command.command.actionFn(...args, options);
   }
   throw new RuntimeError('There is no matched command');
 }
