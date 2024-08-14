f06bf00578e12bce62fd25af054385db156e1c53
yjl9903 <yjl9903@onekuma.cn>
2024-08-14 17:18:34 +0800

    feat(core): option parser

--- packages/core/src/parser/context.ts
+++ packages/core/src/parser/context.ts
@@ -8,7 +8,7 @@ export interface Container {
   commands: ICommand[];
 }
 
-export interface ContextMetadata {}
+export interface ContextMetadata { }
 
 export class Context {
   /**
@@ -63,10 +63,10 @@ export class Context {
      */
     options: Map<string, IOption>;
   } = {
-    unknown: [],
-    commands: new Map(),
-    options: new Map()
-  };
+      unknown: [],
+      commands: new Map(),
+      options: new Map()
+    };
 
   public constructor(container: Container, args: string[]) {
     this.container = container;
@@ -89,29 +89,109 @@ export class MatchedArgument {
 }
 
 export class MatchedOption {
+  public static FALSE_OPTION = ['false', 'no', 'off'];
+
   public readonly option: IOption;
 
-  public value: any;
+  // TODO: default value, cast value
+
+  public dirty = false;
+
+  public raw: any;
 
   public constructor(option: IOption) {
     this.option = option;
   }
 
+  public get value() {
+    if (this.dirty) {
+      return this.raw;
+    } else {
+      switch (this.option.type) {
+        case 'boolean':
+        case 'optional':
+          return false;
+        case 'required':
+          return undefined;
+        case 'array':
+          return [];
+      }
+    }
+  }
+
   public accept(context: Context, text: string | undefined) {
     switch (this.option.type) {
-      case 'boolean':
+      case 'boolean': {
+        // TODO: support --no-* options
         if (text !== undefined) {
-          // TODO
+          const value = text.toLowerCase();
+          this.raw = MatchedOption.FALSE_OPTION.includes(value) ? false : true;
+        } else {
+          this.raw = true;
         }
-        this.value = true;
         break;
-      case 'optional':
-        // TODO
+      }
+      case 'optional': {
+        // Handle optional options
+        let value = text;
+        if (value === undefined) {
+          const token = context.tokens.peek();
+          if (
+            token &&
+            !token.isEscape &&
+            !token.isLong &&
+            (!token.isShort || token.isNegativeNumber)
+          ) {
+            value = token.toRaw();
+            context.tokens.next();
+          }
+        }
+
+        // Set option value
+        //  1. set option value text
+        //  2. set option true
+        if (value !== undefined) {
+          this.raw = value;
+        } else {
+          this.raw = true;
+        }
+        this.dirty = true;
+
         break;
+      }
       case 'required':
-      case 'array':
-        // TODO
+      case 'array': {
+        // Handle required / array options
+        let value = text;
+        if (value === undefined) {
+          // Try next token
+          const token = context.tokens.peek();
+          if (token && !token.isEscape) {
+            value = token.toRaw();
+            context.tokens.next();
+          } else {
+            // TODO: throw parse error
+            throw new Error('');
+          }
+        }
+
+        // Set option value
+        if (this.option.type === 'required') {
+          if (this.dirty) {
+            // TODO: throw parse error, not support multiple required
+            throw new Error('');
+          }
+          this.raw = value;
+        } else {
+          if (!this.raw) {
+            this.raw = [];
+          }
+          this.raw.push(value);
+        }
+        this.dirty = true;
+
         break;
+      }
     }
   }
 }
