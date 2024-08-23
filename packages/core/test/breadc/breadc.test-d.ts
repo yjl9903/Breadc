00ec0ea4793951fc9a77d7534ad9693665205d40
XLor <yjl9903@vip.qq.com>
2024-08-24 02:37:02 +0800

    feat(core): infer action function type

--- packages/core/test/breadc/breadc.test-d.ts
+++ packages/core/test/breadc/breadc.test-d.ts
@@ -1,7 +1,8 @@
 import { describe, it, expectTypeOf } from 'vitest';
 
-import { Breadc, Option } from '../../src';
-import type { InferOption } from '../../src/breadc/infer';
+import type { InferArgumentsType, InferOption } from '../../src/breadc/infer';
+
+import { Argument, Breadc, Option } from '../../src';
 
 describe('option type infer', () => {
   it('should infer option type', () => {
@@ -35,6 +36,41 @@ describe('option type infer', () => {
   });
 });
 
+describe('argument type infer', () => {
+  it('should infer argument types', () => {
+    expectTypeOf<[]>().toEqualTypeOf<InferArgumentsType<''>>();
+    expectTypeOf<[]>().toEqualTypeOf<InferArgumentsType<'cmd'>>();
+    expectTypeOf<[string]>().toEqualTypeOf<InferArgumentsType<'<cmd>'>>();
+    expectTypeOf<[string, string]>().toEqualTypeOf<
+      InferArgumentsType<'cmd <cmd1> <cmd2>'>
+    >();
+    expectTypeOf<[string, string, string]>().toEqualTypeOf<
+      InferArgumentsType<'cmd <cmd1> <cmd2> <cmd3>'>
+    >();
+    expectTypeOf<[string, string, undefined | string]>().toEqualTypeOf<
+      InferArgumentsType<'cmd <cmd1> <cmd2> [cmd3]'>
+    >();
+    expectTypeOf<
+      [string, string, undefined | string, string[]]
+    >().toEqualTypeOf<
+      InferArgumentsType<'cmd <cmd1> <cmd2> [cmd3] [...cmd4]'>
+    >();
+  });
+
+  it('should reject invalid format', () => {
+    expectTypeOf<never>().toEqualTypeOf<InferArgumentsType<'<cmd1> cmd'>>();
+    expectTypeOf<never>().toEqualTypeOf<InferArgumentsType<'[cmd1] cmd'>>();
+    expectTypeOf<never>().toEqualTypeOf<InferArgumentsType<'[...cmd1] cmd'>>();
+    expectTypeOf<never>().toEqualTypeOf<InferArgumentsType<'[cmd1] <cmd>'>>();
+    expectTypeOf<never>().toEqualTypeOf<
+      InferArgumentsType<'[...cmd1] <cmd>'>
+    >();
+    expectTypeOf<never>().toEqualTypeOf<
+      InferArgumentsType<'[...cmd1] [cmd]'>
+    >();
+  });
+});
+
 describe('breadc app type infer', () => {
   it('should infer option type', () => {
     new Breadc('cli')
@@ -48,6 +84,66 @@ describe('breadc app type infer', () => {
       .addOption(new Option('--flag'))
       .command('')
       .action((option) => {
+        expectTypeOf(option).toEqualTypeOf<{ flag: boolean; '--': string[] }>();
+      });
+  });
+
+  it('should infer argument type', () => {
+    new Breadc('cli')
+      .option('--flag')
+      .command('<name>')
+      .action((name, option) => {
+        expectTypeOf(name).toEqualTypeOf<string>();
+        expectTypeOf(option).toEqualTypeOf<{ flag: boolean; '--': string[] }>();
+      });
+
+    new Breadc('cli')
+      .option('--flag')
+      .command('[name]')
+      .action((name, option) => {
+        expectTypeOf(name).toEqualTypeOf<string | undefined>();
+        expectTypeOf(option).toEqualTypeOf<{ flag: boolean; '--': string[] }>();
+      });
+
+    new Breadc('cli')
+      .option('--flag')
+      .command('[...name]')
+      .action((name, option) => {
+        expectTypeOf(name).toEqualTypeOf<string[]>();
+        expectTypeOf(option).toEqualTypeOf<{ flag: boolean; '--': string[] }>();
+      });
+
+    new Breadc('cli')
+      .option('--flag')
+      .command('cmd1 <cmd2> [cmd3] [...cmd4]')
+      .action((cmd2, cmd3, cmd4, option) => {
+        expectTypeOf(cmd2).toEqualTypeOf<string>();
+        expectTypeOf(cmd3).toEqualTypeOf<string | undefined>();
+        expectTypeOf(cmd4).toEqualTypeOf<string[]>();
+        expectTypeOf(option).toEqualTypeOf<{ flag: boolean; '--': string[] }>();
+      });
+
+    new Breadc('cli')
+      .option('--flag')
+      .command('cmd1 <cmd2>')
+      .argument('[cmd3]')
+      .argument('[...cmd4]')
+      .action((cmd2, cmd3, cmd4, option) => {
+        expectTypeOf(cmd2).toEqualTypeOf<string>();
+        expectTypeOf(cmd3).toEqualTypeOf<string | undefined>();
+        expectTypeOf(cmd4).toEqualTypeOf<string[]>();
+        expectTypeOf(option).toEqualTypeOf<{ flag: boolean; '--': string[] }>();
+      });
+
+    new Breadc('cli')
+      .option('--flag')
+      .command('cmd1 <cmd2>')
+      .addArgument(new Argument('[cmd3]'))
+      .addArgument(new Argument('[...cmd4]'))
+      .action((cmd2, cmd3, cmd4, option) => {
+        expectTypeOf(cmd2).toEqualTypeOf<string>();
+        expectTypeOf(cmd3).toEqualTypeOf<string | undefined>();
+        expectTypeOf(cmd4).toEqualTypeOf<string[]>();
         expectTypeOf(option).toEqualTypeOf<{ flag: boolean; '--': string[] }>();
       });
   });
