32dc032a558f9f5b665c9c4c4032617d8d0bd50d
XLor <yjl9903@vip.qq.com>
2024-08-24 23:27:02 +0800

    perf(core): not create IOption object

--- packages/core/test/breadc/breadc.test.ts
+++ packages/core/test/breadc/breadc.test.ts
@@ -1,9 +1,22 @@
 import { describe, it, expect } from 'vitest';
 
-import { Breadc } from '../../src/index.ts';
+import { parse } from '../../src/parser/index.ts';
+import { Command, makeCommand } from '../../src/breadc/command.ts';
+import { Breadc, Context } from '../../src/index.ts';
 
 describe('breadc', () => {
   it('should create breadc', () => {
     const app = new Breadc('cli', { version: '0.0.0', description: 'app' });
+  });
+
+  it('should not parse duplicated default command', () => {
+    const context = new Context(
+      {
+        globalOptions: [],
+        commands: [makeCommand(new Command('')), makeCommand(new Command(''))]
+      },
+      []
+    );
+    expect(() => parse(context)).toThrow('Find duplicated default command');
   });
 });
