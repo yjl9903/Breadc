8dcf32c2d9407f084068631451aa2a213f490858
XLor <yjl9903@vip.qq.com>
2024-08-24 21:27:44 +0800

    perf(core): lift up format

---
+++ packages/core/scripts/profile.ts
@@ -0,0 +1,30 @@
+import { Breadc } from '../src';
+
+for (let i = 0; i < 10000; i++) {
+  const b = new Breadc('cli');
+  const action = () => {};
+  b.command('[op]').action(action);
+  b.command('dev').action(action);
+  b.command('build <root>').action(action);
+  b.command('preview').action(action);
+  b.command('test [case]').action(action);
+  b.command('run [...args]').action(action);
+
+  b.parse([
+    'run',
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n'
+  ]);
+}
