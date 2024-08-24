46ecb6b097dcf76f39e01e37db6e34013f642cc9
XLor <yjl9903@vip.qq.com>
2024-08-24 20:42:48 +0800

    chore: remove some features

--- packages/core/test/parse.bench.ts
+++ packages/core/test/parse.bench.ts
@@ -104,58 +104,104 @@ describe('Parse more option', () => {
 describe('Parse sub-commands', () => {
   const action = () => {};
 
+  const b = new Breadc('cli');
+  b.command('[op]').action(action);
+  b.command('dev').action(action);
+  b.command('build <root>').action(action);
+  b.command('preview').action(action);
+  b.command('test [case]').action(action);
+  b.command('run [...args]').action(action);
+
   bench('breadc', () => {
-    const b = new Breadc('cli');
-    b.command('[op]').action(action);
-    b.command('dev').action(action);
-    b.command('build <root>').action(action);
-    b.command('preview').action(action);
-    b.command('test [case]').action(action);
-    b.command('run [...args]').action(action);
-
-    b.parse(['op']);
-    b.parse(['dev']);
-    b.parse(['build', 'root']);
-    b.parse(['preview']);
-    b.parse(['test']);
-    b.parse(['test', '1']);
-    b.parse(['run', 'a', 'b', 'c', 'd', 'e']);
+    // b.parse(['op']);
+    // b.parse(['dev']);
+    // b.parse(['build', 'root']);
+    // b.parse(['preview']);
+    // b.parse(['test']);
+    // b.parse(['test', '1']);
+    b.parse([
+      'run',
+      'a',
+      'b',
+      'c',
+      'd',
+      'e',
+      'f',
+      'g',
+      'h',
+      'i',
+      'j',
+      'k',
+      'l',
+      'm',
+      'n'
+    ]);
   });
 
+  const c = cac('cli');
+  c.command('[op]').action(action);
+  c.command('dev').action(action);
+  c.command('build <root>').action(action);
+  c.command('preview').action(action);
+  c.command('test [case]').action(action);
+  c.command('run [...args]').action(action);
   bench('cac', () => {
-    const c = cac('cli');
-    c.command('[op]').action(action);
-    c.command('dev').action(action);
-    c.command('build <root>').action(action);
-    c.command('preview').action(action);
-    c.command('test [case]').action(action);
-    c.command('run [...args]').action(action);
-
-    c.parse(['op']);
-    c.parse(['dev']);
-    c.parse(['build', 'root']);
-    c.parse(['preview']);
-    c.parse(['test']);
-    c.parse(['test', '1']);
-    c.parse(['run', 'a', 'b', 'c', 'd', 'e']);
+    // c.parse(['op']);
+    // c.parse(['dev']);
+    // c.parse(['build', 'root']);
+    // c.parse(['preview']);
+    // c.parse(['test']);
+    // c.parse(['test', '1']);
+    c.parse([
+      'run',
+      'a',
+      'b',
+      'c',
+      'd',
+      'e',
+      'f',
+      'g',
+      'h',
+      'i',
+      'j',
+      'k',
+      'l',
+      'm',
+      'n'
+    ]);
   });
 
+  const d = new Command();
+  d.name('cli');
+  d.argument('[op]').action(action);
+  d.command('dev').action(action);
+  d.command('build <root>').action(action);
+  d.command('preview').action(action);
+  d.command('test [case]').action(action);
+  d.command('run [...args]').action(action);
   bench('commander.js', () => {
-    const d = new Command();
-    d.name('cli');
-    d.argument('[op]').action(action);
-    d.command('dev').action(action);
-    d.command('build <root>').action(action);
-    d.command('preview').action(action);
-    d.command('test [case]').action(action);
-    d.command('run [...args]').action(action);
-
-    d.parse(['node', 'cli', 'op']);
-    d.parse(['node', 'cli', 'dev']);
-    d.parse(['node', 'cli', 'build', 'root']);
-    d.parse(['node', 'cli', 'preview']);
-    d.parse(['node', 'cli', 'test']);
-    d.parse(['node', 'cli', 'test', '1']);
-    d.parse(['node', 'cli', 'run', 'a', 'b', 'c', 'd', 'e']);
+    // d.parse(['node', 'cli', 'op']);
+    // d.parse(['node', 'cli', 'dev']);
+    // d.parse(['node', 'cli', 'build', 'root']);
+    // d.parse(['node', 'cli', 'preview']);
+    // d.parse(['node', 'cli', 'test']);
+    // d.parse(['node', 'cli', 'test', '1']);
+    d.parse([
+      'run',
+      'a',
+      'b',
+      'c',
+      'd',
+      'e',
+      'f',
+      'g',
+      'h',
+      'i',
+      'j',
+      'k',
+      'l',
+      'm',
+      'n'
+    ]);
   });
 });
