e618628e0ec93a77dcd8df4f45f3f853739634ed
XLor <yjl9903@vip.qq.com>
2024-08-31 23:24:31 +0800

    test(core): use addOption and addCommand api

--- packages/core/test/breadc/breadc.test.ts
+++ packages/core/test/breadc/breadc.test.ts
@@ -1,10 +1,28 @@
 import { describe, it, expect } from 'vitest';
 
-import { Breadc } from '../../src/index.ts';
+import { Breadc, Option, Command } from '../../src/index.ts';
 
 describe('breadc', () => {
   it('should create breadc', () => {
     new Breadc('cli', { version: '0.0.0', description: 'app' });
+  });
+
+  it('should create the same option in breadc', () => {
+    const app = new Breadc('cli');
+    app
+      .addOption(new Option('--name <name>'))
+      .addCommand(new Command(''))
+      .action((option) => option.name);
+    expect(app.runSync(['--name', 'name'])).toMatchInlineSnapshot(`"name"`);
+  });
+
+  it('should create the same option in command', () => {
+    const app = new Breadc('cli');
+    app
+      .addCommand(new Command(''))
+      .addOption(new Option('--name <name>'))
+      .action((option) => option.name);
+    expect(app.runSync(['--name', 'name'])).toMatchInlineSnapshot(`"name"`);
   });
 
   it('should not parse duplicated default command', () => {
