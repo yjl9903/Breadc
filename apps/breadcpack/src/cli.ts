9664c6454621af5ed8a016af7972090966457370
yjl9903 <yjl9903@onekuma.cn>
2025-08-24 21:23:15 +0800

    chore: fix ci

--- apps/breadcpack/src/cli.ts
+++ apps/breadcpack/src/cli.ts
@@ -1,9 +1,9 @@
-import { Breadc } from '@breadc/core';
+import { breadc } from 'breadc';
 
 import { version } from '../package.json';
 
-const cli = new Breadc('breadcpack', { version });
+const cli = breadc('breadcpack', { version });
 
-cli.command('[root]', 'Create breadc project').action((root, option) => {});
+cli.command('[root]', 'Create breadc project').action((root, option) => { });
 
 cli.run(process.argv.slice(2)).catch((err) => console.error(err));
