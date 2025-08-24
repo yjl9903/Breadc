5f096eec40ccac9bc0df2eba4ae5904772dc644a
yjl9903 <yjl9903@onekuma.cn>
2025-08-24 21:27:53 +0800

    chore: fix package.json

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     includeSource: ['src/**/*.{js,ts}'],
+    exclude: ['legacy/**/*.{js,ts}'],
     coverage: {
       provider: 'v8',
       reportsDirectory: 'coverage',
