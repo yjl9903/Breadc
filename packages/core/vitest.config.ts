6c0e9eef0f9dd7eea7152359905d4bc0a5e4d597
yjl9903 <yjl9903@onekuma.cn>
2026-02-05 03:18:06 +0800

    chore: fix coverage

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -8,8 +8,7 @@ export default defineConfig({
       '**\/dist/**',
       '**\/cypress/**',
       '**\/.{idea,git,cache,output,temp}/**',
-      '**\/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
-      '**\/legacy/**'
+      '**\/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*'
     ],
     typecheck: {
       exclude: [
@@ -17,34 +16,14 @@ export default defineConfig({
         '**\/dist/**',
         '**\/cypress/**',
         '**\/.{idea,git,cache,output,temp}/**',
-        '**\/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
-        '**\/legacy/**'
+        '**\/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*'
       ]
     },
     coverage: {
       provider: 'v8',
       reportsDirectory: 'coverage',
       reporter: ['text', 'html', 'json', 'lcov'],
-      exclude: [
-        '**/mod.ts',
-        'coverage/**',
-        'dist/**',
-        'legacy/**',
-        '**/[.]**',
-        'packages/*/test?(s)/**',
-        '**/*.d.ts',
-        '**/virtual:*',
-        '**/__x00__*',
-        '**/\x00*',
-        'cypress/**',
-        'test?(s)/**',
-        'test?(-*).?(c|m)[jt]s?(x)',
-        '**/*{.,-}{test,spec}.?(c|m)[jt]s?(x)',
-        '**/__tests__/**',
-        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
-        '**/vitest.{workspace,projects}.[jt]s?(on)',
-        '**/.{eslint,mocha,prettier}rc.{?(c|m)js,yml}'
-      ]
+      include: ['src/**/*.ts']
     }
   }
 });
